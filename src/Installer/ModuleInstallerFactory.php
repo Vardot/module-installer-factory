76c746c92775a5912a9dd3825870af20bf46d973
Rajab Natshah <rajabn@gmail.com>
2022-03-28 17:53:44 +0300

    Add a new method ModuleInstallerFactory::importConfigsFromList #4

--- src/Installer/ModuleInstallerFactory.php
+++ src/Installer/ModuleInstallerFactory.php
@@ -17,8 +17,11 @@ class ModuleInstallerFactory {
    * Install a list of modules inside [$moduleName].info.yml
    *
    * @param string $moduleName
+   *   The machine name for the module.
    * @param string $modulesListKey
+   *   Optional list key which to get the list of modules from. Default 'install'.
    * @param bool $setModuleWeight
+   *   A flag to auto set the weight of the module after installation of list of modules.
    * @return void
    */
   public static function installList(string $moduleName, string $modulesListKey = 'install', $setModuleWeight = TRUE) {
@@ -42,7 +45,20 @@ class ModuleInstallerFactory {
     }
   }
 
-  public static function setModuleWeightAfterInstallation(string $moduleName, string $modulesListKey = 'install', $modules = []) {
+  /**
+   * Set the weight of the module after installation of list of modules.
+   * 
+   * To make sure that any hook or event subscriber workes after all used modules.
+   *
+   * @param string $moduleName
+   *   The machine name for the module.
+   * @param string $modulesListKey
+   *   Optional list key which to get the list of modules from. Default 'install'.
+   * @param array $modules
+   *   Optional list of modules in an array.
+   * @return void
+   */
+  public static function setModuleWeightAfterInstallation(string $moduleName, string $modulesListKey = 'install', array $modules = []) {
     if (empty($modules)) {
       $modulePath = \Drupal::service('module_handler')->getModule($moduleName)->getPath();
       $moduleInfoFile = $modulePath . '/' . $moduleName . '.info.yml';
@@ -75,8 +91,12 @@ class ModuleInstallerFactory {
    * Import configuration from scaned directory.
    *
    * @param string $moduleName
+   *   The machine name for the module.
    * @param string $mask
+   *   The mask regular expression format to scan with.
    * @param string $configDirectory
+   *   The config directory which to partial import using the
+   *   mask regular expression format.
    * @return void
    */
   public static function importConfigsFromScanedDirectory(string $moduleName, string $mask, string $configDirectory = InstallStorage::CONFIG_OPTIONAL_DIRECTORY) {
@@ -99,6 +119,32 @@ class ModuleInstallerFactory {
           $configFactory = \Drupal::service('config.factory')->getEditable($configFile->name);
           $configFactory->setData($configFileData)->save(TRUE);
         }
+      }
+    }
+  }
+
+  /**
+   * Import configuration from array list of config files.
+   *
+   * @param string $moduleName
+   *   The machine name for the module.
+   * @param array $listOfConfigFiles
+   *   The list of config files.
+   * @param string $configDirectory
+   *   The config directory which to partial import the list from.
+   * @return void
+   */
+  public static function importConfigsFromList(string $moduleName, array $listOfConfigFiles, string $configDirectory = InstallStorage::CONFIG_OPTIONAL_DIRECTORY) {
+    $modulePath = \Drupal::service('module_handler')->getModule($moduleName)->getPath();
+    $configPath = "{$modulePath}/{$configDirectory}";
+
+    if (is_dir($configPath)) {
+      foreach ($listOfConfigFiles as $configName) {
+        $configPath = $configPath . '/' . $configName . '.yml';
+        $configContent = file_get_contents($configPath);
+        $configData = (array) Yaml::parse($configContent);
+        $configFactory = \Drupal::configFactory()->getEditable($configName);
+        $configFactory->setData($configData)->save(TRUE);
       }
     }
   }
